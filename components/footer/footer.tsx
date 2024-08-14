176c2232dd76abdf0d19ae9d2a0273eb961caa40
IamCharlesM <because@charlesisa.dev>
2024-08-14 15:55:48 -0500

    initial layout

---
+++ components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center w-full p-4 bg-gray-800 text-white">
+      <p>&copy; 2021 My App</p>
+    </footer>
+  );
+}
