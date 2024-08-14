baa0dbb7d43b0804bdd93f2f0556838bd0b7b66c
IamCharlesM <because@charlesisa.dev>
2024-08-14 17:23:30 -0500

    Add navbar

--- components/nav/navBar.tsx
+++ components/nav/navBar.tsx
@@ -3,13 +3,14 @@ import { Button } from "../ui/button";
 export default function NavBar() {
   return (
     <nav className="flex items-center justify-between w-full p-4 bg-gray-800 text-white">
-      <h1 className="text-2xl font-bold">My App</h1>
-      <ul className="flex space-x-4">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <Button className=" bg-primary">Test</Button>
+      <div className="flex w-fit h-full">
+        {" "}
+        <h1 className="text-2xl font-bold">My App</h1>
+      </div>
+      <div className="flex w-fit h-full">
+        <Button variant={"ghost"}>reset</Button>
+        <Button className=" bg-primary">resume</Button>
+      </div>
     </nav>
   );
 }
